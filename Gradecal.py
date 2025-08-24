8595bc6a3899435bf1b4f8d52eb56aa233973da3
Artpytnin <piyatat71@gmail.com>
2025-08-24 18:56:08 +0700

    Create Gradecal.py

---
+++ Gradecal.py
@@ -0,0 +1,23 @@
+# Input the score
+
+score = float(input("Enter the student's score: "))
+
+# Determine the grade based on the score
+
+if score >= 90  :
+    grade = 'A'
+
+elif score >= 80 :
+    grade = 'B'
+
+elif score >= 70 :
+    grade = 'C'
+
+elif score >= 60 :
+    grade = 'D'
+
+else:
+    grade = 'F'
+
+
+print(grade)
