6d7a51fdcc9228f22050e512852495df1f3bd5ac
Artpytnin <piyatat71@gmail.com>
2025-08-24 18:50:37 +0700

    Create BMI.py

---
+++ BMI.py
@@ -0,0 +1,20 @@
+fullname = input("Enter your name : ")
+
+
+
+
+
+weight = float(input("Enter your weights (kg) : "))
+height = float(input("Enter your heights (cm) : "))
+bmi = weight / ((height / 100) ** 2)
+
+print("%s.Your bmi is %f" % (fullname, bmi))
+
+
+
+
+#weight = 84 
+#height = 1.84
+#bmi = weight / height ** 2
+
+#print(bmi)
